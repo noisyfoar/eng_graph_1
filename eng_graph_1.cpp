69dada88da7be8d9a9ce197a12a28a4e6ad1cf80
saladd <spad1239@gmail.com>
2023-03-03 19:15:04 +0500

    1st lesson

--- eng_graph_1.cpp
+++ eng_graph_1.cpp
@@ -1,11 +1,31 @@
-﻿#include <iostream>
-#include<GLFW/glfw3.h>
-#include<GL/freeglut.h>
-#include<GL/glew.h>
+﻿#include <GL/freeglut.h>
 
-int main()
+
+static void RenderSceneCB()
 {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+static void InitializeGlutCallbacks()
+{
+    glutDisplayFunc(RenderSceneCB);
+}
+
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowSize(1280, 720);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Tutorial 1");
+    glutInitWindowPosition(640, 360);
+    glutCreateWindow("Lesson 1");
+
+    InitializeGlutCallbacks();
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    glutMainLoop();
+
+    return 0;
 }
