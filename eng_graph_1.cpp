6f737dd7308b4788e5691a59ccedcd193b9cb20d
saladd <spad1239@gmail.com>
2023-03-03 19:19:24 +0500

    lesson 2

--- eng_graph_1.cpp
+++ eng_graph_1.cpp
@@ -1,15 +1,57 @@
-﻿#include <GL/freeglut.h>
+﻿#include <stdio.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+GLuint VBO;
+
+struct Vector3f
+{
+    float x;
+    float y;
+    float z;
+
+    Vector3f()
+    {
+    }
+
+    Vector3f(float _x, float _y, float _z)
+    {
+        x = _x;
+        y = _y;
+        z = _z;
+    }
+};
 
 
 static void RenderSceneCB()
 {
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glDrawArrays(GL_POINTS, 0, 1);
+
+    glDisableVertexAttribArray(0);
+
     glutSwapBuffers();
 }
+
 
 static void InitializeGlutCallbacks()
 {
     glutDisplayFunc(RenderSceneCB);
+}
+
+static void CreateVertexBuffer()
+{
+    Vector3f Vertices[1];
+    Vertices[0] = Vector3f(0.0f, 0.0f, 0.0f);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 }
 
 
@@ -18,12 +60,20 @@ int main(int argc, char** argv)
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowSize(1280, 720);
-    glutInitWindowPosition(640, 360);
-    glutCreateWindow("Lesson 1");
+    glutInitWindowPosition(960, 540);
+    glutCreateWindow("Lesson 2");
 
     InitializeGlutCallbacks();
 
+    GLenum res = glewInit();
+    if (res != GLEW_OK) {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
+
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    CreateVertexBuffer();
 
     glutMainLoop();
 
