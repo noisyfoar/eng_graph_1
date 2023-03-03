f862775ee8ccf1a9a26947a8f8e5b1198aa7b890
saladd <spad1239@gmail.com>
2023-03-03 19:00:56 +0500

    Добавьте файлы проекта.

---
+++ eng_graph_1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include<GLFW/glfw3.h>
+#include<GL/freeglut.h>
+#include<GL/glew.h>
+
+int main()
+{
+    glutInitWindowSize(1280, 720);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Tutorial 1");
+}
